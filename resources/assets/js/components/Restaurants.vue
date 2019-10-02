aa165e7254147c1030c5e66dc3e214a45cfb4e92
Karthik Korapatti <karthik@motoinnov.com>
2019-10-02 19:13:06 +0530

    Changed the banner

--- resources/assets/js/components/Restaurants.vue
+++ resources/assets/js/components/Restaurants.vue
@@ -3,11 +3,12 @@
  	<portal	to="topBanner">
 		<!-- SubHeader =============================================== -->
 		<section class="parallax-window" id="short" data-parallax="scroll"
+			style="background-image: url(/images/banner.png); background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover;" 
 			data-image-src="img/sub_header_short.jpg" data-natural-width="1400" data-natural-height="350">
 		<div id="subheader">
 			<div id="sub_content">
-				<h1>{{ restaurants.length }} results in your zone</h1>
-				<div><i class="icon_pin"></i> 135 Newtownards Road, Belfast, BT4 1AB</div>
+				<h1>Welcome to Mother's Serve</h1>
+				<div><i class="icon_pin"></i> Rajarajeshwari Nagar, Bengaluru.</div>
 			</div><!-- End sub_content -->
 		</div><!-- End subheader -->
 		</section><!-- End section -->
